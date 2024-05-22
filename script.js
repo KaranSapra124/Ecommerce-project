a36c6a5623a3b68f8684c20edb9ff8be1ad42d95
Karan sapra <88654249+KaranSapra124@users.noreply.github.com>
2024-05-22 11:09:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const allProducts = document.getElementById("allProducts");
 const productDesc = document.getElementById("productDesc");
+const similarProducts = document.getElementById("similarProducts");
 const id = new URLSearchParams(window.location.search);
 const val = id.get("id");
+let productCategory = "";
 
 if (allProducts) {
   fetchProducts("https://dummyjson.com/products");
@@ -57,9 +59,12 @@ function fetchSingleProduct(url) {
 }
 
 function displayIndividualProduct(obj) {
+  fetchProductsByCategory(
+    `https://dummyjson.com/products/category/${obj.category}`
+  );
   productDesc.innerHTML = `
   
- <div> <img  src=${obj.images[0]} />
+ <div> <img  src=${obj.thumbnail} />
  <button class="bg-yellow-400 p-2 mt-2 rounded text-white font-semibold hover:bg-yellow-500" hover:>Add to Cart </button>
   
  <button class="bg-green-400 p-2 mt-2 rounded text-white font-semibold hover:bg-green-500" hover:>Buy Now </button>
@@ -90,6 +95,10 @@ function displayIndividualProduct(obj) {
   }">${Math.round(obj.discountPercentage)}% Off</span>
 
 </div>
+<span class="bg-green-100 w-fit text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400">${
+    obj.category
+  }</span>
+
 <div><span class="font-bold mx-2">Description:</span>${obj.description}</div>
 
 
@@ -102,6 +111,52 @@ function displayIndividualProduct(obj) {
   `;
 }
 
+const fetchProductsByCategory = (url) => {
+  fetch(url)
+    .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+      displayRelatedProducts(data.products);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 fetchSingleProduct(`https://dummyjson.com/products/${val}`);
+
+const displayRelatedProducts = (arr) => {
+  const filteredArr = arr.filter((elem) => {
+    return elem.id !== +val;
+  });
+  filteredArr.map((elem) => {
+    return (similarProducts.innerHTML += `<div>
+    <img class="w-52 h-52 object-cover" src='${elem?.thumbnail}'/>
+    <h3>${elem.title}</h3>
+    <span class="${
+      elem.rating < 5
+        ? "bg-green-500 text-white w-fit text-sm  p-1 rounded "
+        : "text-white  p-1 bg-green-600"
+    }">
+    ⭐ ${elem.rating}
+  </span>
+  <p class="text-2xl font-semibold" >₹${
+    elem.discountPercentage
+      ? Math.round(
+          elem.price - (elem.price * Math.round(elem.discountPercentage)) / 100
+        )
+      : elem.price
+  }</p>
+<p class="${
+      elem.discountPercentage ? "line-through mx-2 text-gray-400" : "hidden"
+    }" >₹${elem.price}</p>
+  <span class="${
+    elem.discountPercentage ? " mx-2 text-green-500 font-semibold" : "hidden"
+  }">${Math.round(elem.discountPercentage)}% Off</span>
+
+    </div>`);
+  });
+};
 
 // Students data school
